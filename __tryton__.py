05f3a9a509bb4eb1409ff6d1e49e4536efeec178
Cédric Krier <ced@b2ck.com>
2010-05-09 01:23:41 +0200

    Increase version number

--- __tryton__.py
+++ __tryton__.py
@@ -6,7 +6,7 @@
     'name_es_CO': 'Traducción de Google',
     'name_es_ES': 'Google Traductor',
     'name_fr_FR': 'Google Translate',
-    'version' : '1.6.0',
+    'version' : '1.6.1',
     'author' : 'B2CK',
     'email': 'info@b2ck.com',
     'website': 'http://www.tryton.org/',
